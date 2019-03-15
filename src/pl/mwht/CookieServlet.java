135eaadf30b609bf04e5df2c945d1db601ac58ff
Sebastian Madejski <209388@edu.p.lodz.pl>
2019-03-15 09:40:02 +0100

    initial commit

---
+++ src/pl/mwht/CookieServlet.java
@@ -0,0 +1,24 @@
+package pl.mwht;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "CookieServlet")
+public class CookieServlet extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.getWriter().println("CookieServlet here");
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        processRequest(request, response);
+    }
+}
