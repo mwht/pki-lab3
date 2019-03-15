9df615fe039bf291680cee4b8df30580b3481e9e
Sebastian Madejski <209388@edu.p.lodz.pl>
2019-03-15 09:43:50 +0100

    add session stub

--- src/pl/mwht/CookieServlet.java
+++ src/pl/mwht/CookieServlet.java
@@ -5,13 +5,25 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet(name = "CookieServlet")
 public class CookieServlet extends HttpServlet {
 
     protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.getWriter().println("CookieServlet here");
+        PrintWriter out = response.getWriter();
+        HttpSession session = request.getSession(true);
+
+        Boolean loggedIn = (Boolean) session.getAttribute("zalogowany");
+        try {
+            if (loggedIn == null) {
+                loggedIn = false;
+            }
+        } finally {
+            out.close();
+        }
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
