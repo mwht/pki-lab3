86c244ab21ace9773b1486d87f907f98607538c1
Sebastian Madejski <209388@edu.p.lodz.pl>
2019-03-20 17:01:02 +0100

    add initial implementation of counter

--- src/pl/mwht/CounterServlet.java
+++ src/pl/mwht/CounterServlet.java
@@ -2,15 +2,47 @@ package pl.mwht;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet(name = "CounterServlet")
 public class CounterServlet extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter out = response.getWriter();
 
+        try {
+
+            Cookie[] cookies = request.getCookies();
+            Cookie licznik = null;
+            if (cookies != null) {
+                for (int i = 0; i < cookies.length; i++) {
+                    if (cookies[i].getName().equals("licznik")) {
+                        licznik = cookies[i];
+                        break;
+                    }
+                }
+            }
+
+            if (licznik == null) {
+                licznik = new Cookie("licznik", "0");
+            } else {
+                Integer v = Integer.parseInt(licznik.getValue());
+                v++;
+                licznik.setValue(v + "");
+            }
+
+            licznik.setMaxAge(-1);
+            response.addCookie(licznik);
+
+            out.println(licznik.getValue());
+        } finally {
+            out.flush();
+            out.close();
+        }
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
--- web/index.jsp
+++ web/index.jsp
@@ -11,6 +11,6 @@
     <title>$Title$</title>
   </head>
   <body>
-  <a href="CookieServlet">Go to servlet</a>
+  <a href="CounterServlet">Go to counter servlet</a>
   </body>
 </html>
