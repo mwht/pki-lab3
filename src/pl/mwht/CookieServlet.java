0c31d42d1809b83e5a4bd58c231813a025f778db
Sebastian Madejski <209388@edu.p.lodz.pl>
2019-03-20 16:43:51 +0100

    add validation of username and password

--- src/pl/mwht/CookieServlet.java
+++ src/pl/mwht/CookieServlet.java
@@ -16,19 +16,30 @@ public class CookieServlet extends HttpServlet {
         PrintWriter out = response.getWriter();
         HttpSession session = request.getSession(true);
 
+        response.setContentType("text/html; charset=utf-8");
+
         Boolean loggedIn = (Boolean) session.getAttribute("zalogowany");
         try {
-            if (loggedIn == null || !loggedIn) {
+            if (loggedIn == null || !loggedIn) { // not logged in
                 loggedIn = false;
                 out.println("<form method=\"GET\">\n" +
                         "<input type=\"text\" name=\"user\" />\n" +
                         "<input type=\"password\" name=\"pass\" />\n" +
                         "<input type=\"submit\" value=\"zaloguj\" />\n" +
                         "</form>\n");
-            } else if (loggedIn) {
-                // TODO: logged in
-            } else {
 
+                String user, pass;
+                user = request.getParameter("user");
+                pass = request.getParameter("pass");
+
+                if(user != null && pass != null) {
+                    if(user.equals("admin") && pass.equals("admin1")) {
+                        loggedIn = true;
+                        session.setAttribute("zalogowany", loggedIn);
+                    }
+                }
+            } else { // logged in
+                // TODO: logged in
             }
         } finally {
             out.close();
